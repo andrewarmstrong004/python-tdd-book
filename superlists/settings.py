28a6ab03923b099847b1df5abd7a914147c30cda
Andrew Armstrong <andrewarmstrong004@gmail.com>
2022-07-06 23:31:31 -0500

    hack ALLOWED_HOSTS to be *

--- superlists/settings.py
+++ superlists/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '95s!wf$z)@*_$q6@#wne7_t-31*i!=votz#pa7&r1xwx6mr2-('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
